e655aadf25a1c91abfd7340397ded81397e159a0
Sefa Arzuman <82059064+sefaarzuman@users.noreply.github.com>
2021-04-06 23:02:44 +0300

    Add files via upload

---
+++ 1-HesapMak.py
@@ -0,0 +1,15 @@
+sayi1 = int(input ("1. sayiyi gir: "))
+sayi2 = int(input ("2. sayiyi gir: "))
+
+islem = input ("islemi sec(+, -, *, /):")
+if islem == "+":
+   print("sonuc:", sayi1 + sayi2)
+
+elif islem == "-":
+    print("sonuc:", sayi1 - sayi2)
+
+elif islem == "*":
+    print("sonuc:", sayi1 * sayi2)
+
+elif islem == "/":
+    print("sonuc:", sayi1 / sayi2)
